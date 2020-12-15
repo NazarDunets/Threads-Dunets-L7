bfb7f6ee28c0976f13f5812ad46146ddbfb5c595
NazarDunets <jcq.nazardunets@gmail.com>
2020-12-15 17:46:04 +0200

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,26 @@
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicInteger
+
+var counter = 0
+
+fun main(args: Array<String>) {
+
+    // This works, but probably just because "counter ++" is such a quick operation
+    // But... Since we depend on time precision and not number of operations this looks like the best way to go
+
+    val scheduledEx = Executors.newScheduledThreadPool(5)
+
+    scheduledEx.scheduleAtFixedRate({println(counter)},
+        0,1000,TimeUnit.MILLISECONDS)
+
+    scheduledEx.scheduleAtFixedRate({counter ++},
+        0,10,TimeUnit.MILLISECONDS)
+    scheduledEx.scheduleAtFixedRate({counter ++},
+        0,10,TimeUnit.MILLISECONDS)
+    scheduledEx.scheduleAtFixedRate({counter ++},
+        0,10,TimeUnit.MILLISECONDS)
+    scheduledEx.scheduleAtFixedRate({counter ++},
+        0,10,TimeUnit.MILLISECONDS)
+
+}
