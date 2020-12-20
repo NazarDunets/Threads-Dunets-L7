52541f05f86aaf5da5b9875cbbc5e4646a010f69
NazarDunets <75180459+NazarDunets@users.noreply.github.com>
2020-12-20 17:57:48 +0200

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,12 +2,11 @@ import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicInteger
 
-var counter = 0
-
 fun main(args: Array<String>) {
 
     // This works, since "counter ++" is such a quick operation
     // But... Since we depend on time precision and not number of operations this looks like the best way to go
+    var counter = 0
 
     val scheduledEx = Executors.newScheduledThreadPool(5)
 
